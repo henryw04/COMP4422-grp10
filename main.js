9051b57a656a98ca1fa97af2f545fc74252bd5d5
Henry04 <henryyy.wong04@gmail.com>
2024-11-27 22:01:54 +0800

    createPlanet function with tilt support

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from './lib/three-module.js';
 import {OrbitControls} from './lib/OrbitControls.js';
+import { CreatePlanet } from './lib/CreatePlanet.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,7 +9,7 @@ camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+
 
 document.body.appendChild( renderer.domElement );
 
@@ -16,19 +17,20 @@ new OrbitControls(camera,renderer.domElement);
 
 const loader = new THREE.TextureLoader();
 
-const geometry = new THREE.IcosahedronGeometry(1,12);
-const material = new THREE.MeshBasicMaterial( { 
-	map: loader.load("./textures/earthmap1k.jpg"), 
-} );
+//preload texture
+const suntexture = loader.load('./textures/sunmapthumb.jpg');
+const earthtexture = loader.load('./textures/earthmap1k.jpg');
 
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
+//sun = CreatePlanet(suntexture,scene);
+const earth = CreatePlanet(1,earthtexture,-23.4);
+scene.add(earth);
 
 function animate() {
 
-	cube.rotation.y += 0.01;
+	earth.rotateY(0.01);
 
 	renderer.render( scene, camera );
 
 }
+renderer.setAnimationLoop(animate);
